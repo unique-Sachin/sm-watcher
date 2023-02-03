d8ef04abaa0c20ab5e426011f3f3e422b18e2684
Sachin Mishra <msachin786.pm@gmail.com>
2023-02-03 21:25:23 +0530

    changed the cli

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
-const file = process.argv[2]
+const file = process.argv[2];
+
 import { fork } from "child_process";
 import chokidar from "chokidar";
 import chalk from "chalk";
-console.log(chalk.blue.bgRed.bold("UPAR WALA SAB DEKH RAHA HAI"));
+console.log(chalk.blue.bgRed.bold("sm-watch server started"));
 const watcher = chokidar.watch("./", {
   ignoreInitial: true,
 });
 
 let child = fork("./" + file);
-let count = 1;
 
 function restartChildProcess() {
-  console.log(
-    chalk.green("Bhai kya kar raha hai ") + count + chalk.green(" bar ho gya")
-  );
-  count++;
+  console.log(chalk.green("sm-watch server restarted...."));
   child.kill();
   child = fork("./" + file);
 }
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 const app = express();
 app.get("/", (req, res) => {
   res.write("hiiii");
-  res.write("sachin Mishra");
+  res.write("sachin Mishra \n");
+  res.write("Hanumat Sharan ");
   res.send();
 });
 app.listen(8080);
