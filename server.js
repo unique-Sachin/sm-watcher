78cd0fb796f7469a8ca559be82ae85a2b6592854
Sachin Mishra <msachin786.pm@gmail.com>
2023-02-03 22:02:55 +0530

    updated

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-app.get("/", (req, res) => {
-  res.write("hiiii");
-  res.write("sachin Mishra \n");
-  res.write("Hanumat Sharan ");
-  res.send();
-});
-app.listen(8080);
